eab6774ce1e3748094b180db54dd2195344473ab
Ice P4nda <brunom.tania@gmail.com>
2025-09-25 21:50:27 +0100

    2nd commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scripted_Service
+
+Developed with Unreal Engine 5 by me
---
+++ Source/Scripted_Service/Scripted_Service.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Scripted_Service.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Scripted_Service, "Scripted_Service" );
