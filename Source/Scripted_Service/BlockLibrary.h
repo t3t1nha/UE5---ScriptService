d1c80e42464a793481ae275b880150397b81550e
Ice P4nda <brunom.tania@gmail.com>
2026-02-04 20:27:21 +0000

    .

---
+++ Source/Scripted_Service/BlockLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BlockLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCRIPTED_SERVICE_API UBlockLibrary : public UObject
+{
+	GENERATED_BODY()
+	
+};
