3ac23c818782778627b81daa9026a28171e88ac1
Ice P4nda <brunom.tania@gmail.com>
2026-03-05 00:52:12 +0000

    .

---
+++ Source/Scripted_Service/UI/ProgramSequenceWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ProgramSequenceWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCRIPTED_SERVICE_API UProgramSequenceWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
